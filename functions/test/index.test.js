b5ed7384540f2d9b4b0c59087eb68e47773fcde0
Adonis Arifi <adonisarifi@gmail.com>
2018-07-05 11:13:02 +0200

    update

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,3 +1,8 @@
-test('Don\'t try to convert non-images', () => {
-   console.log('just test')
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
   });
+});
