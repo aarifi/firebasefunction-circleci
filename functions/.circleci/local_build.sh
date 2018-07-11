a2e0014e74ae21f2a30a72321d7546ad765567b4
Adonis Arifi <adonisarifi@gmail.com>
2018-07-11 14:24:03 +0200

    update

--- functions/.circleci/local_build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=c83d9e364b7ad79e2bd60edca56f32aaa8069e8f \
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/aarifi/firebasefunction-circleci/tree/master
